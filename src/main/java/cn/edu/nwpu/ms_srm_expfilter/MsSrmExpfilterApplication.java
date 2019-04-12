48d19091cf2250f0ebe5dc90af2b57ac245a931e
WangYMY <triumph_wang@outlook.com>
2019-04-12 21:15:12 +0800

    数据滤波初始框架

---
+++ src/main/java/cn/edu/nwpu/ms_srm_expfilter/MsSrmExpfilterApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.nwpu.ms_srm_expfilter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSrmExpfilterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsSrmExpfilterApplication.class, args);
+    }
+
+}
---
+++ src/test/java/cn/edu/nwpu/ms_srm_expfilter/MsSrmExpfilterApplicationTests.java
@@ -0,0 +1,16 @@
+package cn.edu.nwpu.ms_srm_expfilter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MsSrmExpfilterApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
